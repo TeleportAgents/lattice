2b675bc933a5bb206a6773154c1584339257aae6
Amirhossein Nouranizadeh <anouranizadeh@interrodata.com>
2024-08-12 02:32:28 +0330

    dev-#9 bm25 search scores added. codes organized.

--- src/lattice/retrieve/retriever.py
+++ src/lattice/retrieve/retriever.py
@@ -1,12 +1,16 @@
 import os
 import ast
-from typing import Tuple, Union
+import hashlib
+import configparser
+import json
+from typing import Tuple, Dict, Union
 
 from dotenv import load_dotenv
 import numpy as np
 import pandas as pd
 from together import Together
 from openai import OpenAI
+from rank_bm25 import BM25Okapi
 
 from src.lattice.llm.together import get_together_chat_response, get_together_embedding
 from paths import ROOT_PROJECT_PATH
@@ -20,144 +24,166 @@ def get_db_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
     return entity, relationship
 
 
-def normalize_embeddings(x: np.ndarray) -> np.ndarray:
-    norms = np.linalg.norm(x, axis=1, keepdims=True)
-    normalized_embeddings = x / norms
-    return normalized_embeddings
+def get_entity_embeddings(
+        entity: pd.DataFrame,
+        num_keywords: int
+) -> np.ndarray:
+    # loading function_name embeddings
+    function_name_embeddings = np.stack(entity["function_name_embedding"].apply(ast.literal_eval))
+    function_name_embeddings = np.expand_dims(function_name_embeddings, axis=1)
+
+    # loading definition embeddings
+    definition_embeddings = np.stack(entity["definition_embedding"].apply(ast.literal_eval))
+    definition_embeddings = np.expand_dims(definition_embeddings, axis=1)
+
+    # loading description embeddings
+    description_embeddings = np.stack(entity["description_embedding"].apply(ast.literal_eval))
+    description_embeddings = np.expand_dims(description_embeddings, axis=1)
+
+    # loading keyword embeddings
+    keyword_embeddings = []
+    for idx in range(num_keywords):
+        keyword_idx_embeddings = np.stack(entity[f"keyword_{idx}_embedding"].apply(ast.literal_eval))
+        keyword_idx_embeddings = np.expand_dims(keyword_idx_embeddings, axis=1)
+        keyword_embeddings.append(keyword_idx_embeddings)
+    keyword_embeddings = np.concat(keyword_embeddings, axis=1)
+
+    # packing all embeddings together
+    entity_embeddings = np.concat(
+        [function_name_embeddings, definition_embeddings, description_embeddings, keyword_embeddings],
+        axis=1
+    )
+    return entity_embeddings
+
+
+def bm25_search(
+        query: str,
+        entity: pd.DataFrame
+) -> np.ndarray:
+    documents = (entity['description'] + '\n' + entity['definition']).tolist()
+
+    # Convert the document-term matrix to a list of lists (sparse matrix to dense)
+    tokenized_docs = [doc.split() for doc in documents]
+
+    # Tokenize the query in the same way
+    tokenized_query = query.split()
+
+    # Initialize BM25 with the tokenized documents
+    bm25 = BM25Okapi(tokenized_docs)
+
+    # Get the BM25 scores for the query against all documents
+    scores = bm25.get_scores(tokenized_query)
+    return scores
 
 
 def retrieve(
         query: str,
-        llm_client: Union[Together],
+        num_keywords: int,
+        keyword_extraction_prompt_template: str,
+        together_llm_client: Union[Together],
         together_llm_model_name: str,
-        embedding_client: Union[OpenAI],
+        together_embedding_client: Union[OpenAI],
         together_embedding_model_name: str,
         score_type: str = "normalized_inner_product_score",
 
-):
+) -> Dict:
     assert score_type in ["normalized_inner_product_score", "normalized_l2_score"], \
         "The argument score_type must be either 'normalized_inner_product_score' or 'normalized_l2_score'."
 
-    # loading data
+    # loading entity and relation data
     entity, relationship = get_db_data()
 
-    # processing the embeddings
-    function_name_embeddings = np.stack(entity["function_name_embedding"].apply(ast.literal_eval))
-    function_name_embeddings = normalize_embeddings(x=function_name_embeddings)
-    definition_embeddings = np.stack(entity["definition_embedding"].apply(ast.literal_eval))
-    definition_embeddings = normalize_embeddings(x=definition_embeddings)
-    description_embeddings = np.stack(entity["description_embedding"].apply(ast.literal_eval))
-    description_embeddings = normalize_embeddings(x=description_embeddings)
+    # Loading the embeddings of data
+    entity_embeddings = get_entity_embeddings(entity=entity, num_keywords=num_keywords)
 
-    # preparing the query
-    info_extraction_prompt_1 = f"""
-    You are knowledgeable in retrieving Python function based on natural language queries of user.
-    I will give you the users query. I will also give you function names and their description. 
-    Tell which 2 functions are most relevant or similar to user's query.
-    
-    QUERY:
-    {query}
-    
-    FUNCTIONS NAME:
-    {entity["function_name"].values.tolist()}
-    
-    FUNCTION DESCRIPTIONS:
-    {entity["description"].values.tolist()}
-    """
-    num_keywords = 20
-    info_extraction_prompt_2 = f"""
-    You are knowledgeable in retrieving Python function based on natural language queries of user.
-    I will give you the users query. 
-    I want you to grasp the query entirely, and output {num_keywords} keywords that reflect the essence of the query.
-    These {num_keywords} keywords can be keywords in the query, or synonyms of the key concepts in the query.
-    I want to embed these {num_keywords} keywords and use the embeddings to compute similarities in a RAG system.
-    Output the {num_keywords} keywords in a Python list.
-    DO NOT OUTPUT EXTRA EXPLANATIONS OTHER THAN THE PYTHON LIST.
-    
-    QUERY:
-    {query}
-    
-    PYTHON LIST:
-    """
-    response = get_together_chat_response(prompt=info_extraction_prompt_2,
-                                          together_llm_client=llm_client,
-                                          together_llm_model_name=together_llm_model_name)
-    query_keywords = ast.literal_eval(response)
-    query_keywords_embeddings = []
-    for keyword in query_keywords:
-        keyword_embedding = get_together_embedding(text=keyword,
-                                                   together_embedding_client=embedding_client,
-                                                   together_model_name=together_embedding_model_name)
-        keyword_embedding = np.expand_dims(np.array(keyword_embedding), axis=0)
-        keyword_embedding = normalize_embeddings(x=keyword_embedding)
-        query_keywords_embeddings.append(keyword_embedding.squeeze().tolist())
-    query_keywords_embeddings = np.array(query_keywords_embeddings)
+    # query processing
+    query_hash_object = hashlib.md5(query.encode())
+    query_hash = query_hash_object.hexdigest()
+    query_path = f"query_{query_hash}.csv"
+    if os.path.isfile(query_path):
+        # loading query embeddings and keywords
+        query_data = pd.read_csv(query_path)
 
-    keywords_results = entity.copy()
-    score_cols = []
-    for keyword_idx in range(len(query_keywords)):
-        emb = query_keywords_embeddings[keyword_idx, :]
-        if score_type == 'normalized_inner_product_score':
-            function_name_scores = (function_name_embeddings @ emb.T).squeeze()
-            definition_scores = (definition_embeddings @ emb.T).squeeze()
-            description_scores = (description_embeddings @ emb.T).squeeze()
-        elif score_type == 'normalized_l2_score':
-            function_name_scores = 2 - np.linalg.norm(function_name_embeddings - emb, axis=1)
-            definition_scores = 2 - np.linalg.norm(definition_embeddings - emb, axis=1)
-            description_scores = 2 - np.linalg.norm(description_embeddings - emb, axis=1)
-        else:
-            raise ValueError
-        keywords_results[f"function_name_scores@{query_keywords[keyword_idx]}"] = function_name_scores
-        keywords_results[f"definition_scores@{query_keywords[keyword_idx]}"] = definition_scores
-        keywords_results[f"description_scores@{query_keywords[keyword_idx]}"] = description_scores
-        score_cols.append(f"function_name_scores@{query_keywords[keyword_idx]}")
-        score_cols.append(f"definition_scores@{query_keywords[keyword_idx]}")
-        score_cols.append(f"description_scores@{query_keywords[keyword_idx]}")
-
-    pass
-
-    # processing the query
-    # embedding the query
-    query_embedding = get_together_embedding(text=query,
-                                             together_embedding_client=embedding_client,
-                                             together_model_name=together_embedding_model_name)
-    query_embedding = np.expand_dims(np.array(query_embedding), axis=0)
-    query_embedding = normalize_embeddings(x=query_embedding)
-
-    if score_type == 'normalized_inner_product_score':
-        function_name_scores = (function_name_embeddings @ query_embedding.T).squeeze()
-        definition_scores = (definition_embeddings @ query_embedding.T).squeeze()
-        description_scores = (description_embeddings @ query_embedding.T).squeeze()
-    elif score_type == 'normalized_l2_score':
-        function_name_scores = 2 - np.linalg.norm(function_name_embeddings - query_embedding, axis=1)
-        definition_scores = 2 - np.linalg.norm(definition_embeddings - query_embedding, axis=1)
-        description_scores = 2 - np.linalg.norm(description_embeddings - query_embedding, axis=1)
     else:
-        raise ValueError
+        # extracting keywords of the query
+        query_prompt = keyword_extraction_prompt_template.format(description=query, code="")
+        query_keywords = get_together_chat_response(prompt=query_prompt,
+                                                    together_llm_client=together_llm_client,
+                                                    together_llm_model_name=together_llm_model_name)
+        query_keywords = json.loads(query_keywords)
+        query_keywords = query_keywords['description_keywords']
 
-    results = entity.copy()
-    results["function_name_scores"] = function_name_scores
-    results["definition_scores"] = definition_scores
-    results["description_scores"] = description_scores
-    pass
+        # embedding keywords of the query
+        query_data = {
+            "keyword": [],
+            "embedding": []
+        }
+        for query_keyword_idx, query_keyword in enumerate(query_keywords):
+            query_keyword_emb = get_together_embedding(text=query_keyword,
+                                                       together_embedding_client=together_embedding_client,
+                                                       together_model_name=together_embedding_model_name,
+                                                       normalize_embedding=True)
+            query_data["keyword"].append(query_keyword)
+            query_data["embedding"].append(str(query_keyword_emb))
+        query_data = pd.DataFrame.from_dict(data=query_data)
+        query_data.to_csv(query_path, index=False)
+
+    # loading query keyword embeddings
+    query_embeddings = np.stack(query_data["embedding"].apply(ast.literal_eval))
+
+    # computing the similarity score between entity embeddings and query embeddings
+    emb_scores = entity_embeddings @ query_embeddings.T
+
+    # ============== computing the retrieved entity ==============
+    # flattening the score query scores for each entity
+    emb_scores = emb_scores.reshape(emb_scores.shape[0], -1)
+    # averaging the two highest scores for each entity
+    emb_scores = np.sort(emb_scores, axis=1)[:, -2:].mean(1)
+
+    # computing the BM25 search scores
+    bm25_scores = bm25_search(query=query, entity=entity)
+
+    # constructing score dataframe
+    score = pd.DataFrame.from_dict(data={"embedding": emb_scores.tolist(), "bm25": bm25_scores}).reset_index()
+
+    # first getting the top-5 BM25 search scores, then return the top embedding scores among those top-5 scores
+    result_idx = int(
+        score.sort_values(by="bm25",
+                          ascending=False).iloc[:9].sort_values(by="embedding", ascending=False).iloc[0]['index']
+    )
+    result = entity.iloc[result_idx][["id", "function_name", "description", "definition"]].to_dict()
+    return result
 
 
 def main():
+    # configurations
     load_dotenv()
     together_api_key = os.getenv('TOGETHER_API_KEY')
     together_llm_client = Together(api_key=together_api_key)
-    embedding_client = OpenAI(api_key=together_api_key, base_url="https://api.together.xyz/v1")
+    together_embedding_client = OpenAI(api_key=together_api_key, base_url="https://api.together.xyz/v1")
     together_llm_model_name = "meta-llama/Llama-3-70b-chat-hf"
     together_embedding_model_name = "togethercomputer/m2-bert-80M-32k-retrieval"
 
+    # reading prompt templates
+    config = configparser.ConfigParser()
+    config.read(os.path.join(ROOT_PROJECT_PATH, 'src', 'lattice', 'retrieve', 'prompt.ini'))
+    keyword_extraction_prompt_template = config['prompts']['keyword_extraction']
+
+    # calling the retrieve function
     query = "I want a function that visualizes a histogram."
-    retrieve(query=query,
-             llm_client=together_llm_client,
-             together_llm_model_name=together_llm_model_name,
-             embedding_client=embedding_client,
-             together_embedding_model_name=together_embedding_model_name,
-             score_type="normalized_l2_score")
-    pass
+    num_keywords = 10
+    result = retrieve(query=query,
+                      num_keywords=num_keywords,
+                      keyword_extraction_prompt_template=keyword_extraction_prompt_template,
+                      together_llm_client=together_llm_client,
+                      together_llm_model_name=together_llm_model_name,
+                      together_embedding_client=together_embedding_client,
+                      together_embedding_model_name=together_embedding_model_name,
+                      score_type="normalized_l2_score")
+    print("Input Query:")
+    print(query)
+    print("Result:")
+    print(json.dumps(result, indent=4))
 
 
 if __name__ == "__main__":
